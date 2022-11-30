a331c3d86cb15a8584f8be97bf3ba64bf448da63
Nobel Zhou <nobelzhou19@gmail.com>
2022-11-30 16:21:03 -0500

    Moved getEntity methods to separate class file

---
+++ src/MMLTools.java
@@ -0,0 +1,88 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+public class MMLTools {
+    public static Playlist getPlaylist(Connection connection, int userID, int playlistID, boolean canEdit) throws SQLException {
+        String sql = "{call get_playlist_by_id (" + userID + ", " + playlistID + ")}";
+        CallableStatement callableStatement = connection.prepareCall(sql);
+        ResultSet playlistResultSet = callableStatement.executeQuery();
+
+        if (playlistResultSet.next()) {
+            User user = getUser(connection, userID);
+
+            sql = "{call get_songs_by_playlist (" + userID + ", " + playlistID + ")}";
+            callableStatement = connection.prepareCall(sql);
+
+            ResultSet songResultSet = callableStatement.executeQuery();
+
+            LinkedList<Song> songList = new LinkedList<>();
+            int j = 0;
+            while (songResultSet.next()) {
+                songList.add(getSong(connection, songResultSet.getInt(1)));
+                j++;
+            }
+
+            Song[] songs = new Song[j];
+            songList.toArray(songs);
+
+            return new Playlist(user, playlistID, playlistResultSet.getInt("duration_value"),
+                    playlistResultSet.getString("duration"), playlistResultSet.getString("name"),
+                    songs, canEdit);
+        } else
+            throw new SQLException("Playlist not found");
+    }
+
+    public static User getUser(Connection connection, int userID) throws SQLException {
+        String sql = "{call get_user_by_id (" + userID + ")}";
+        CallableStatement callableStatement = connection.prepareCall(sql);
+
+        ResultSet resultSet = callableStatement.executeQuery();
+
+        if (resultSet.next())
+            return new User(userID, resultSet.getString("username"), resultSet.getString("name"),
+                    resultSet.getString("join_date"));
+        else
+            throw new SQLException("User not found");
+    }
+
+    public static Song getSong(Connection connection, int songID) throws SQLException {
+        String sql = "{call get_song_by_id (" + songID + ")}";
+        CallableStatement callableStatement = connection.prepareCall(sql);
+
+        ResultSet songResultSet = callableStatement.executeQuery();
+
+        if (songResultSet.next()) {
+            sql = "{call get_artists_by_song (" + songID + ")}";
+            callableStatement = connection.prepareCall(sql);
+
+            ResultSet artistResultSet = callableStatement.executeQuery();
+
+            LinkedList<Artist> artistList = new LinkedList<>();
+            int j = 0;
+            while (artistResultSet.next()) {
+                artistList.add(getArtist(connection, artistResultSet.getInt(1)));
+                j++;
+            }
+
+            Artist[] artists = new Artist[j];
+            artistList.toArray(artists);
+
+            if (artists.length == 0)
+                throw new SQLException("No artists found for song");
+
+            return new Song(songID, songResultSet.getInt("duration"),
+                    songResultSet.getString("duration_string"),
+                    songResultSet.getString("release_date"), songResultSet.getString("name"),
+                    artists);
+        } else
+            throw new SQLException("Song not found");
+    }
+
+    public static Artist getArtist(Connection connection, int artistID) throws SQLException {
+        //TODO implement
+        throw new UnsupportedOperationException();
+    }
+}
--- src/MainCLI.java
+++ src/MainCLI.java
@@ -172,7 +172,7 @@ public class MainCLI {
 
     private static int mainMenu(Connection connection, int userID) {
         try {
-            User user = getUser(connection, userID);
+            User user = MMLTools.getUser(connection, userID);
             String name = user.getName();
 
             // TODO change when getArtist() is implemented
@@ -219,7 +219,7 @@ public class MainCLI {
                     while (playlistID == 0) {
                         playlistID = playlistMenu(connection, userID);
                         if (playlistID > 0) {
-                            viewPlaylist(connection, userID, getPlaylist(connection, userID, playlistID, true));
+                            viewPlaylist(connection, userID, MMLTools.getPlaylist(connection, userID, playlistID, true));
                             playlistID = 0;
                         }
                     }
@@ -376,7 +376,7 @@ public class MainCLI {
             int i = 0;
             while (resultSet.next()) {
                 int playlistID = resultSet.getInt(1);
-                Playlist playlist = getPlaylist(connection, userID, playlistID, true);
+                Playlist playlist = MMLTools.getPlaylist(connection, userID, playlistID, true);
                 playlistList.add(playlist);
 
                 if (i < 9)
@@ -513,86 +513,5 @@ public class MainCLI {
             scanner.nextLine();
             return -1;
         }
-    }
-
-    private static Playlist getPlaylist(Connection connection, int userID, int playlistID, boolean canEdit) throws SQLException {
-        String sql = "{call get_playlist_by_id (" + userID + ", " + playlistID + ")}";
-        CallableStatement callableStatement = connection.prepareCall(sql);
-        ResultSet playlistResultSet = callableStatement.executeQuery();
-
-        if (playlistResultSet.next()) {
-            User user = getUser(connection, userID);
-
-            sql = "{call get_songs_by_playlist (" + userID + ", " + playlistID + ")}";
-            callableStatement = connection.prepareCall(sql);
-
-            ResultSet songResultSet = callableStatement.executeQuery();
-
-            LinkedList<Song> songList = new LinkedList<>();
-            int j = 0;
-            while (songResultSet.next()) {
-                songList.add(getSong(connection, songResultSet.getInt(1)));
-                j++;
-            }
-
-            Song[] songs = new Song[j];
-            songList.toArray(songs);
-
-            return new Playlist(user, playlistID, playlistResultSet.getInt("duration_value"),
-                    playlistResultSet.getString("duration"), playlistResultSet.getString("name"),
-                    songs, canEdit);
-        } else
-            throw new SQLException("Playlist not found");
-    }
-
-    private static User getUser(Connection connection, int userID) throws SQLException {
-        String sql = "{call get_user_by_id (" + userID + ")}";
-        CallableStatement callableStatement = connection.prepareCall(sql);
-
-        ResultSet resultSet = callableStatement.executeQuery();
-
-        if (resultSet.next())
-            return new User(userID, resultSet.getString("username"), resultSet.getString("name"),
-                    resultSet.getString("join_date"));
-        else
-            throw new SQLException("User not found");
-    }
-
-    private static Song getSong(Connection connection, int songID) throws SQLException {
-        String sql = "{call get_song_by_id (" + songID + ")}";
-        CallableStatement callableStatement = connection.prepareCall(sql);
-
-        ResultSet songResultSet = callableStatement.executeQuery();
-
-        if (songResultSet.next()) {
-            sql = "{call get_artists_by_song (" + songID + ")}";
-            callableStatement = connection.prepareCall(sql);
-
-            ResultSet artistResultSet = callableStatement.executeQuery();
-
-            LinkedList<Artist> artistList = new LinkedList<>();
-            int j = 0;
-            while (artistResultSet.next()) {
-                artistList.add(getArtist(connection, artistResultSet.getInt(1)));
-                j++;
-            }
-
-            Artist[] artists = new Artist[j];
-            artistList.toArray(artists);
-
-            if (artists.length == 0)
-                throw new SQLException("No artists found for song");
-
-            return new Song(songID, songResultSet.getInt("duration"),
-                    songResultSet.getString("duration_string"),
-                    songResultSet.getString("release_date"), songResultSet.getString("name"),
-                    artists);
-        } else
-            throw new SQLException("Song not found");
-    }
-
-    private static Artist getArtist(Connection connection, int artistID) throws SQLException {
-        //TODO implement
-        throw new UnsupportedOperationException();
     }
 }
